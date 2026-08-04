bc70931b57cafe1c64a5b0d0e44497b1c69faa2b
smanisha-27 <smanisha0701@gmail.com>
2026-08-04 19:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (47.85%) - LeetSync

---
+++ 61-rotate-list/rotate-list.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rotateRight(self, head, k):
+        if not head or not head.next or k == 0:
+            return head
+
+        # Find length and last node
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        # Reduce unnecessary rotations
+        k = k % length
+        if k == 0:
+            return head
+
+        # Make circular list
+        tail.next = head
+
+        # Find new tail
+        steps = length - k
+        new_tail = head
+
+        for _ in range(steps - 1):
+            new_tail = new_tail.next
+
+        # Break the cycle
+        new_head = new_tail.next
+        new_tail.next = None
+
+        return new_head
